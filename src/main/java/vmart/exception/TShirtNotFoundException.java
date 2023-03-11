674a2188d4299720088f7f216d486123345949b4
Chetan <chetan@Chetans-MacBook-Pro.local>
2023-03-11 19:13:24 +0530

    Updating codes

---
+++ src/main/java/vmart/exception/TShirtNotFoundException.java
@@ -0,0 +1,11 @@
+package vmart.exception;
+
+public class TShirtNotFoundException extends Exception {
+    public TShirtNotFoundException() {
+        super();
+    }
+
+    public TShirtNotFoundException(String msg) {
+        super(msg);
+    }
+}
